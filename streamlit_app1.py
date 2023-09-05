8ccad26c5b869c8fed8a0fc14685b325f97fc615
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2023-09-04 22:36:47 -0600

    Update streamlit_app1.py

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -8,6 +8,18 @@ st.text('Sentence number 1. Sentence number 2')
 header = st.container()
 dataset = st.container()
 features = st.container()
+model_training = st.container()
 with header:
   st.title('Welcome to my project')
+  st.text('The following is a description of my project')
+
+with dataset:
+  st.header('NYC taxi dataset')
+  st.text('this dataset is from...')
+  
+with features:  
+  st.header('These are the features:')
+
+with model_training:
+  st.header('Time to train the model!')
   
