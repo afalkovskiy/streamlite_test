756e2c956939ea63ba76c961066f97fa7c597d9b
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2023-09-04 23:50:47 -0600

    Update streamlit_app1.py

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -1,36 +1,15 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
+#import numpy as np
+
+st.title('first streamlit 1')
 
 rand = np.random.normal(1, 2, size=20)
 fig, ax = plt.subplots()
 ax.hist(rand, bins=15) #, color="pink"
 st.pyplot(fig)
 
-st.title('first streamlit 1')
 
-st.header('This is my header')
-st.text('Sentence number 1. Sentence number 2')
 
-header = st.container()
-dataset = st.container()
-features = st.container()
-model_training = st.container()
-with header:
-  st.title('Welcome to my project')
-  st.text('The following is a description of my project')
-
-with dataset:
-  st.header('NYC taxi dataset')
-  st.text('this dataset is from...')
-
-  #taxi_data = pd.read_csv('data/taxi_data.csv')
-  #st.write(taxi_data.head())
-  
-with features:  
-  st.header('These are the features:')
-
-with model_training:
-  st.header('Time to train the model!')
   
