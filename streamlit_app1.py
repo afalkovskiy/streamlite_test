0edc52a4507404928cb449eca0a4fed4e882e18b
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2023-09-04 22:10:29 -0600

    Update streamlit_app1.py

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -1,6 +1,8 @@
-import streamlit
+import streamlit as st
 
-streamlit.title('first streamlit')
+streamlit.title('first streamlit 1')
 
 streamlit.header('This is my header')
 streamlit.text('Sentence number 1. Sentence number 2')
+
+header = st
