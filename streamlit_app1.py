9538092599a6df2ff05e90dea5ff9472b41f9c4a
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2023-09-04 23:58:00 -0600

    Update streamlit_app1.py

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -1,14 +1,14 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-#import numpy as np
+#import matplotlib.pyplot as plt
+import numpy as np
 
 st.title('first streamlit 1')
 
 rand = np.random.normal(1, 2, size=20)
-fig, ax = plt.subplots()
-ax.hist(rand, bins=15) #, color="pink"
-st.pyplot(fig)
+#fig, ax = plt.subplots()
+#ax.hist(rand, bins=15) #, color="pink"
+#st.pyplot(fig)
 
 
 
