c50fe6394839068afc1fc04a285c83e5711e62f1
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2023-09-04 22:26:32 -0600

    Update streamlit_app1.py

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -5,7 +5,7 @@ st.title('first streamlit 1')
 st.header('This is my header')
 st.text('Sentence number 1. Sentence number 2')
 
-header = st.beta_container()
+header = st.container()
 with header:
   st.title('Welcome to my project')
   
