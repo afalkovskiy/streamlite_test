436a8616ebb9060b3ebaf6224688e711e45666d9
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2023-09-13 23:04:33 -0600

    Update chart.py

--- chart.py
+++ chart.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-df = pd.DataFrame(np.random.random(10,2),
-columns=['x','y'])
+df = pd.DataFrame(
+  np.random.random(10,2),
+  columns=['x','y'])
 st.area_chart(df)
