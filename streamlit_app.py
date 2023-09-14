a08c3657a388fef09754d0c3d1783398bb283ec4
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2023-09-13 20:45:58 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 # import matplotlib.pyplot as plt
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import numpy as np
 
 st.title('first streamlit 1')
