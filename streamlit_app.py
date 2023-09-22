30a597574e72cca33d6bf850fbffa2cf34b2979b
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2023-09-22 11:49:31 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,17 +8,18 @@ st.title('Plot sin(x)')
 #st.button('Hit me')
 
 x = np.arange(0, 4*np.pi, 0.1)
-y = np.sin(x)
+sinx = np.sin(x)
+cosx = np.cos(x)
 
 chart_data = pd.DataFrame(
    {
-       "col1": np.random.randn(20),
-       "col2": np.random.randn(20),
-       "col3": np.random.choice(["A", "B", "C"], 20),
+       "x": x,
+       "sin(x)": sinx,
+       "cos(x)": cosx,
    }
 )
 
-st.line_chart(chart_data, x="col1", y="col2", color="col3")
+st.line_chart(chart_data, x="x", y="f(x)", color="col3")
 
 
 rand = np.random.normal(1, 2, size=20)
