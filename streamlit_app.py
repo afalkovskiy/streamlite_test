41afe7b541e42c57de492ec7d8c21e0bc42080a5
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2023-09-22 14:42:22 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,7 @@ import numpy as np
 st.title('Plot sin(x)')
 #st.button('Hit me')
 st.subheader("f(x) = a*sin(b(x+c)) + d")
+a = st.slider('Select a from [-10, 10]', min_value=0.0, max_value=10.)
 
 x = np.arange(0, 4*np.pi, 0.1)
 sinx = np.sin(x)
