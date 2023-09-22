1af30076a04bfc6ddba908b6da027352b94564d2
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2023-09-22 11:29:15 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,9 @@ import numpy as np
 st.title('first streamlit 1')
 st.button('Hit me')
 
+x = np.arrange(0, 4*np.pi, 0.1)
+y = np.sin(x)
+
 rand = np.random.normal(1, 2, size=20)
 fig, ax = plt.subplots()
 ax.hist(rand, bins=15) #, color="pink"
