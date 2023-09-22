256b12c5fb91581c12d60b98e5bb9ecf49c4f368
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2023-09-22 11:35:56 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 st.title('first streamlit 1')
-st.button('Hit me')
+#st.button('Hit me')
 
-x = np.arrange(0, 4*np.pi, 0.1)
+x = np.arange(0, 4*np.pi, 0.1)
 y = np.sin(x)
 
 rand = np.random.normal(1, 2, size=20)
