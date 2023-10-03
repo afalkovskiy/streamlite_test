1837997fd44f3196d0209e2094ffa16d66b4e094
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2023-10-03 16:23:54 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import pandas as pd
 # import matplotlib.pyplot as plt
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.signal import hilbert
 
 st.title('Plot sin(x)')
 #st.button('Hit me')
