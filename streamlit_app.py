8c5e68b9afb40fd5bd51d9389bac24930dc5bd0d
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2023-10-05 10:42:53 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from scipy.signal import hilbert
 
 st.title('Plot sin(x)')
 #st.button('Hit me')
-st.subheader("f(x) = A*sin(B(x+C)) + D, Oct 3, 2023")
+st.subheader("f(x) = A*sin(B(x+C)) + D, Oct 5, 2023")
 a = st.slider('Select a value of A from [-10, 10]', value=1., min_value=-10., max_value=10.)
 st.write("A = ", a)
 
