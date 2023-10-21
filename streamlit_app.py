0cdcb40a0af5d11d6853f596da74ff392f3e36e8
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2023-10-20 22:20:44 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,6 +50,7 @@ chart_data = pd.DataFrame(
 
 st.line_chart(chart_data, x="x", y=["sin","cos", "x_rot"] )
 st.line_chart(chart_data, x="x", y=["sin"] )
+st.line_chart(chart_data, x="sin", y=["x"] )
 
 rand = np.random.normal(1, 2, size=20)
 fig, ax = plt.subplots()
