d6e1702cf97205b380a5e7e07ffada2af324329e
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2024-08-17 19:11:27 -0600

    nCos

--- test_plot.py
+++ test_plot.py
@@ -5,37 +5,23 @@ import plotly_express as px
 
 st.title('Test plots')
 
+nCol = 6
 
-fig = make_subplots(rows=1, cols=2, shared_yaxes=True)
+fig = make_subplots(rows=1, cols=nCol, shared_yaxes=True)
 
-fig.add_trace(
-    go.Scatter(x=[1, 2, 3, 4], y=[4, 5, 6, 85]),
-    row=1, col=1
-)
+for i in range(nCol/2):
+    fig.add_trace(
+        go.Scatter(x=[1, 2, 3, 4], y=[4, 5, 6, 85]),
+        row=1, col=i*2+1
+    )
 
-fig.add_trace(
-    go.Scatter(x=[20, 30, 40], y=[50, 60, 70]),
-    row=1, col=2
-)
+    fig.add_trace(
+        go.Scatter(x=[20, 30, 40], y=[50, 60, 70]),
+        row=1, col=i*2 + 2
+    )
 
 fig.update_layout(height=600, width=800, title_text="Side By Side Subplots")
 # fig.show()
 st.plotly_chart(fig, theme="streamlit", use_container_width=True)
 
-fig1 = make_subplots(rows=2, cols=2, start_cell="bottom-left")
 
-fig1.add_trace(go.Scatter(x=[1, 2, 3], y=[4, 5, 6]),
-              row=1, col=1)
-
-fig1.add_trace(go.Scatter(x=[20, 30, 40], y=[50, 60, 70]),
-              row=1, col=2)
-
-fig1.add_trace(go.Scatter(x=[300, 400, 500], y=[600, 700, 800]),
-              row=2, col=1)
-
-fig1.add_trace(go.Scatter(x=[4000, 5000, 6000], y=[7000, 8000, 9000]),
-              row=2, col=2)
-
-fig1.update_layout(height=600, width=800, title_text="Multiple subplots")
-# fig.show()
-st.plotly_chart(fig1, theme="streamlit", use_container_width=True)
