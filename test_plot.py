1a889ed1cd40b8c776280078205133a417668b16
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2024-08-18 00:05:26 -0600

    c

--- test_plot.py
+++ test_plot.py
@@ -20,10 +20,11 @@ for i in range(nCol):
         row=1, col=i+1
     )
 
+    fig.layout.annotations[i].update(text="Subtitle " + str(i+1))
 
 
 fig.update_layout(height=600, width=800, title_text="Side By Side Subplots")
-fig.layout.annotations[2].update(text="Stackoverflow")
+# fig.layout.annotations[2].update(text="Stackoverflow")
 # fig.show()
 st.plotly_chart(fig, theme="streamlit", use_container_width=True)
 
